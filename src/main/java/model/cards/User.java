903e7e2e6feb13324837457af6a9021e2fe6025e
alibnz <alibnz.2001@gmail.com>
2021-04-13 19:05:07 +0430

    user model

---
+++ src/main/java/model/cards/User.java
@@ -0,0 +1,47 @@
+package model.cards;
+
+public class User {
+    private String username;
+    private String password;
+    private String nickname;
+    private int Score;
+
+    public User(String username, String password, String nickname, int score) {
+        setUsername(username);
+        setPassword(password);
+        setNickname(nickname);
+        setScore(score);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public int getScore() {
+        return Score;
+    }
+
+    public void setScore(int score) {
+        Score = score;
+    }
+}
