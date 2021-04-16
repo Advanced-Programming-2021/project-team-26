8af9eecdb4f6ad043b27bee1359c82bcb32c29a2
Abolfazl <g.o.d.abolfazlm@gmail.com>
2021-04-16 18:47:35 +0430

    shopMenu

---
+++ src/main/java/view/menus/ShopMenu.java
@@ -0,0 +1,19 @@
+package model;
+
+import model.cards.Card;
+
+public class ShopMenu {
+    private static HashMap <String/*NAME*/, Integer/*PRICE*/> cardsPrice = new HashMap<>();
+
+    public ShopMenu(){
+    }
+
+    static {
+        //TODO
+        //setCardsPrice
+    }
+
+    public boolean isExistThisCardName(String name){
+        return true;
+    }
+}
