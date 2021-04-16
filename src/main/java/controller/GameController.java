f4aa9903cea90938c313ca851d5a4e46e9368c75
mohammad <maghaie14@yahoo.com>
2021-04-16 12:38:30 +0430

    add GameController

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -1,4 +1,71 @@
 package controller;
 
+import model.Game;
+
 public class GameController {
+    private Game game;
+
+    public GameController(String firstPlayer,String secondPayer,int round){
+
+    }
+
+    public GameController(String player,int round){
+
+    }
+
+    public void select(String address){
+
+    }
+
+    public void summon(){
+
+    }
+
+    public void set(){
+
+    }
+
+    public void set(String position){
+
+    }
+
+    public void flipSummon(){
+
+    }
+
+    public void attack(int number){
+
+    }
+
+    public void attack(){
+
+    }
+
+    public void activateEffect(){
+
+    }
+
+    public void showGraveyard(){
+
+    }
+
+    public void cardShow(){
+
+    }
+
+    public void surrender(){
+
+    }
+
+    public boolean isFinished(){
+        return false;
+    }
+
+    public void endGame(){
+
+    }
+
+    public void showBoard(){
+
+    }
 }
