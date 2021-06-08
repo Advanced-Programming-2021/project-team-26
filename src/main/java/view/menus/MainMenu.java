50b6972f405ff38a3c9970662e4e591397c2f2eb
mohammad <maghaie14@yahoo.com>
2021-06-08 13:15:22 +0430

    fix some bugs

--- src/main/java/view/menus/MainMenu.java
+++ src/main/java/view/menus/MainMenu.java
@@ -1,15 +1,11 @@
 package view.menus;
 
-import controller.GameController;
 import controller.MainMenuController;
 import controller.UserController;
 import view.ConsumerSp;
 import view.Menu;
 
-import java.lang.reflect.Method;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Consumer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -19,13 +15,10 @@ public class MainMenu extends Menu {
     static {
         MAIN_MENU_CONTROLLER = new MainMenuController();
         MAP.put(Pattern.compile("^\\s*user logout\\s*$"), UserController.getInstance()::logout);
-        MAP.put(Pattern.compile("^\\s*menu show-current\\s*$"), i -> {
-            return "Main menu";
-        });
+        MAP.put(Pattern.compile("^\\s*menu show-current\\s*$"), i -> "Main menu");
         MAP.put(Pattern.compile("^\\s*menu exit\\s*$"), Menu::exitMenu);
         MAP.put(Pattern.compile("^\\s*menu enter ([^ ]+)\\s*$"),MAIN_MENU_CONTROLLER::enterMenu );
         MAP.put(Pattern.compile("^\\s*duel (.+)\\s*$"), MAIN_MENU_CONTROLLER :: createNewGameWithRealPlayer);
-        MAP.put(Pattern.compile("^\\s*duel (.+)\\s*$"), MAIN_MENU_CONTROLLER :: creatNewGameWithAI );
     }
 
     public void execute() {
