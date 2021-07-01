3400a28191689561ef78cc8f19fbdff70050a0b3
alibnz <alibnz.2001@gmail.com>
2021-07-01 19:34:36 +0430

    score board completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 //import view.menus.LoginMenu;
-
-public class Main {
-    public static void main(String[] args) {
-        //new LoginMenu().execute();
-    }
-}
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        new LoginMenu().execute();
+//    }
+//}
--- src/main/java/fxmlController/Scoreboard.java
+++ src/main/java/fxmlController/Scoreboard.java
@@ -1,44 +1,49 @@
 package fxmlController;
 
 import controller.ScoreBoardController;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
-import model.User;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import static fxmlController.Welcome.getFXML;
 
-public class Scoreboard {
+public class Scoreboard implements Initializable {
+
+    @FXML
+    private TableView<ScoreBoardController> table;
+    @FXML
+    private TableColumn<ScoreBoardController, Integer> rank;
+    @FXML
+    private TableColumn<ScoreBoardController, String> name;
+    @FXML
+    private TableColumn<ScoreBoardController, Integer> score;
+
     public void run() throws IOException {
         Stage primaryStage = Welcome.getStage();
         Parent root = getFXML("scoreboard");
         primaryStage.setTitle("Score Board");
         primaryStage.setScene(new Scene(root, Welcome.WIDTH, Welcome.HEIGHT));
         primaryStage.show();
-
-        ArrayList<User> sortedUsers = new ScoreBoardController().sortUsersBasedScore();
-        showScoreBoard(sortedUsers);
     }
 
-    @FXML
-    private TableView<?> table;
-
-    @FXML
-    private TableColumn<?, ?> rank;
-
-    @FXML
-    private TableColumn<?, ?> name;
-
-    @FXML
-    private TableColumn<?, ?> score;
-
-    private void showScoreBoard(ArrayList<User> sortedUsers) {
-        //TODO
+    @Override
+    public void initialize(URL location, ResourceBundle resourceBundle) {
+        rank.setCellValueFactory(new PropertyValueFactory<>("rank"));
+        name.setCellValueFactory(new PropertyValueFactory<>("name"));
+        score.setCellValueFactory(new PropertyValueFactory<>("score"));
+        ScoreBoardController.getAndSetDataFromUser();
+        ObservableList<ScoreBoardController> list = FXCollections.observableArrayList(ScoreBoardController.getScoreBoardControllers());
+        table.setItems(list);
     }
 }
