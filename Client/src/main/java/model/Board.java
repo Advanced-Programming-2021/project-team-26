2eef63db2bb791854ee173d05ae815b2ce19b58e
mohammad <maghaie14@yahoo.com>
2021-07-20 12:51:07 +0430

    clean up

--- Client/src/main/java/model/Board.java
+++ Client/src/main/java/model/Board.java
@@ -2,59 +2,54 @@ package model;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import controller.*;
+import controller.GameController;
+import controller.MonsterTransfer;
+import controller.NetworkController;
+import controller.SpellTrapTransfer;
 import model.cards.Card;
-import model.cards.monster.Monster;
 
 import java.lang.reflect.Type;
 import java.util.*;
 
 public class Board {
-    private List<Card> deck;
     private final GameController gameController;
+    private List<Card> deck;
     private int myTurn;
 
-    public Board(GameController gameController, Deck deck,int myTurn) {
+    public Board(GameController gameController, Deck deck, int myTurn) {
         initDeck(deck);
         this.gameController = gameController;
         this.myTurn = myTurn;
     }
 
     public HashMap<Integer, MonsterTransfer> getMonsterZoneMap() {
-        Request request = new Request("GameController","Board");
-        request.addParameter("turn",myTurn);
-        request.addParameter("function","getMonsterZoneMap");
+        Request request = new Request("GameController", "Board");
+        request.addParameter("turn", myTurn);
+        request.addParameter("function", "getMonsterZoneMap");
         Response response = NetworkController.getInstance().sendAndReceive(request);
-        Type hashMapType = new TypeToken<Map<Integer, MonsterTransfer>>() {}.getType();
-        return new Gson().fromJson(response.getData("MonsterZoneMap"),hashMapType);
+        Type hashMapType = new TypeToken<Map<Integer, MonsterTransfer>>() {
+        }.getType();
+        return new Gson().fromJson(response.getData("MonsterZoneMap"), hashMapType);
     }
 
     public HashMap<Integer, SpellTrapTransfer> getSpellTrapZoneMap() {
-        Request request = new Request("GameController","Board");
-        request.addParameter("turn",myTurn);
-        request.addParameter("function","getSpellTrapZoneMap");
+        Request request = new Request("GameController", "Board");
+        request.addParameter("turn", myTurn);
+        request.addParameter("function", "getSpellTrapZoneMap");
         Response response = NetworkController.getInstance().sendAndReceive(request);
-        Type hashMapType = new TypeToken<Map<Integer, SpellTrapTransfer>>() {}.getType();
-        return new Gson().fromJson(response.getData("SpellTrapZoneMap"),hashMapType);
+        Type hashMapType = new TypeToken<Map<Integer, SpellTrapTransfer>>() {
+        }.getType();
+        return new Gson().fromJson(response.getData("SpellTrapZoneMap"), hashMapType);
     }
 
     public List<Card> getDeck() {
-        Request request = new Request("GameController","Board");
-        request.addParameter("turn",myTurn);
-        request.addParameter("function","getDeck");
+        Request request = new Request("GameController", "Board");
+        request.addParameter("turn", myTurn);
+        request.addParameter("function", "getDeck");
         Response response = NetworkController.getInstance().sendAndReceive(request);
-        Type listType = new TypeToken<List<Card>>() {}.getType();
-        return new Gson().fromJson(response.getData("Deck"),listType);
-    }
-
-    public MonsterTransfer getMonsterByIndex(int index) {
-        //TODO request to server
-        return null;
-    }
-
-    public SpellTrapTransfer getSpellTrapByIndex(int index) {
-        //TODO request to server
-        return null;
+        Type listType = new TypeToken<List<Card>>() {
+        }.getType();
+        return new Gson().fromJson(response.getData("Deck"), listType);
     }
 
     public List<Card> getGraveyard() {
@@ -63,11 +58,6 @@ public class Board {
     }
 
     public List<Card> getHand() {
-        //TODO request to server
-        return null;
-    }
-
-    public SpellTransfer getFieldZone() {
         //TODO request to server
         return null;
     }
