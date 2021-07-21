d705ac5975a2084790a7da21a0c235505cb0841d
Abolfazl <g.o.d.abolfazlm@gmail.com>
2021-07-21 22:30:46 +0430

    a bug fixed

---
+++ Client/src/main/java/fxmlController/Children/AdminPanel.java
@@ -0,0 +1,68 @@
+package fxmlController.Children;
+
+import Utilities.GetFXML;
+import com.google.gson.Gson;
+import controller.Database;
+import controller.NetworkController;
+import controller.ShopController;
+import fxmlController.App;
+import fxmlController.MenuParent;
+import fxmlController.Size;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import model.Request;
+import model.Response;
+import model.User;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+public class AdminPanel extends MenuParent {
+    HashMap<String , String> parameters = new HashMap<>();
+    @FXML
+    public TextField cardName;
+    @FXML
+    public TextField decreaseAmount;
+    @FXML
+    public TextField increaseAmount;
+
+    public AdminPanel() {
+        super("Admin Panel");
+    }
+
+    @Override
+    public void run() throws IOException {
+        Parent root = GetFXML.getFXML("adminPanel");
+        scene = new Scene(root, Size.MAIN_WIDTH.getValue(), Size.MAIN_HEIGHT.getValue());
+        App.pushMenu(this);
+    }
+
+    public void increaseInventory(ActionEvent event) {
+       // parameters.put()
+        Request request = new Request("AdminPanel", "loginUser", parameters);
+        Response response = NetworkController.getInstance().sendAndReceive(request);
+        throw new RuntimeException(response.getMessage());
+    }
+
+    public boolean decreaseInventory(ActionEvent event) {
+        Request request = new Request("AdminPanel", "loginUser", parameters);
+        Response response = NetworkController.getInstance().sendAndReceive(request);
+        throw new RuntimeException(response.getMessage());
+    }
+
+    public boolean disableShopping(ActionEvent event) {
+        Request request = new Request("AdminPanel", "loginUser", parameters);
+        Response response = NetworkController.getInstance().sendAndReceive(request);
+        throw new RuntimeException(response.getMessage());
+    }
+
+    public boolean enableShopping(ActionEvent event) {
+        Request request = new Request("AdminPanel", "loginUser", parameters);
+        Response response = NetworkController.getInstance().sendAndReceive(request);
+        throw new RuntimeException(response.getMessage());
+    }
+
+}
